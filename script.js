47956c92d81bd1495f2e24b22757fc907073d1ca
sizwe <nkosiczwe@gmail.com>
2022-05-02 21:47:49 +0200

    Add DOM elements

---
+++ script.js
@@ -0,0 +1,8 @@
+//DOM Elements
+var hour = document.getElementById("hour");
+var minutes = document.getElementById("minutes");
+var seconds = document.getElementById("seconds");
+
+hour.textContent = hr;
+minutes.textContent = min;
+seconds.textContent = sec;
