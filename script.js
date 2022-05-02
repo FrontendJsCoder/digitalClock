a37d6a34178a24b36119e35d30dd447cf6288359
sizwe <nkosiczwe@gmail.com>
2022-05-02 22:12:29 +0200

    Set time

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //DOM Elements
 var hour = document.getElementById("hour");
-var minutes = document.getElementById("minutes");
+var minutes = document.getElementById("minute");
 var seconds = document.getElementById("seconds");
+
+var clock = setInterval( function time(){
+    var date_now = new Date();
+    var hr = date_now.getHours();
+    var min = date_now.getMinutes();
+    var sec = date_now.getSeconds();
+
+    if(hr < 10){
+        hr = "0" + hr;
+    }
+    if(min < 10){
+        min = "0" + min;
+    }
+    if(sec < 10){
+        sec = "0" + sec;
+    }
+
 
 hour.textContent = hr;
 minutes.textContent = min;
 seconds.textContent = sec;
+},1000
+);
